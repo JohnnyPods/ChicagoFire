b318cd2d5b52dc5b5c804512f3254c115391a5a6
JohnnyPods <johnp@sitetechnologies.io>
2023-08-18 13:25:41 -0500

    Updates

--- precache-manifest.ff81a843ae9d8be1e30e7c7b7e0120b3.js
+++ precache-manifest.ff81a843ae9d8be1e30e7c7b7e0120b3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "7a6fd36a688f4f24cd48",
-    "url": "/ChicagoFire/static/css/main.75b7adf1.chunk.css"
+    "revision": "d7a3016edd91d6f37c3e",
+    "url": "/ChicagoFire/static/css/main.496d3e5e.chunk.css"
   },
   {
-    "revision": "7a6fd36a688f4f24cd48",
-    "url": "/ChicagoFire/static/js/main.7a6fd36a.chunk.js"
+    "revision": "d7a3016edd91d6f37c3e",
+    "url": "/ChicagoFire/static/js/main.d7a3016e.chunk.js"
   },
   {
     "revision": "1a9932cdeed1b6058ba1",
@@ -44,40 +44,56 @@ self.__precacheManifest = [
     "url": "/ChicagoFire/static/media/Colton.e7457b8a.jpeg"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/ChicagoFire/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/ChicagoFire/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/ChicagoFire/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ChicagoFire/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/ChicagoFire/static/media/icons.962a1bf3.svg"
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ChicagoFire/static/media/icons.faff9214.woff"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/ChicagoFire/static/media/icons.0ab54153.woff2"
   },
   {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ChicagoFire/static/media/icons.b87b9ba5.ttf"
+  },
+  {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "/ChicagoFire/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/ChicagoFire/static/media/outline-icons.ad97afd3.ttf"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ChicagoFire/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/ChicagoFire/static/media/outline-icons.701ae6ab.eot"
   },
   {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ChicagoFire/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ChicagoFire/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/ChicagoFire/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ChicagoFire/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ChicagoFire/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -88,24 +104,8 @@ self.__precacheManifest = [
     "url": "/ChicagoFire/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/ChicagoFire/static/media/outline-icons.82f60bd0.svg"
-  },
-  {
     "revision": "a1a749e89f578a49306ec2b055c073da",
     "url": "/ChicagoFire/static/media/brand-icons.a1a749e8.svg"
-  },
-  {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/ChicagoFire/static/media/icons.faff9214.woff"
-  },
-  {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/ChicagoFire/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/ChicagoFire/static/media/flags.9c74e172.png"
   },
   {
     "revision": "5dba2e987d0e01fa089d03fd1abb514d",
@@ -188,7 +188,7 @@ self.__precacheManifest = [
     "url": "/ChicagoFire/static/media/TeamPhoto.051cd575.jpg"
   },
   {
-    "revision": "1b9cc6cc616c7f43fc5a0feaa6a8cb10",
+    "revision": "03229fc851cc4085c1fb8ad34c1092a1",
     "url": "/ChicagoFire/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ChicagoFire/precache-manifest.1f595ba9434c19287f35624e5a103c97.js"
+  "/ChicagoFire/precache-manifest.ff81a843ae9d8be1e30e7c7b7e0120b3.js"
 );
 
 workbox.clientsClaim();
